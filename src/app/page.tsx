30c4bc29375e2d3ea1b9c56be58f9edfdf63b198
dileeparanawake <dileeparanawake@icloud.com>
2025-07-15 15:29:41 +0100

    update theme global.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,8 @@ import { MousePointerClick } from 'lucide-react';
 export default function ChatPage() {
   return (
     <div className="h-screen flex items-center justify-center">
-      <h1>Title</h1>
       <Button variant="default" size="lg">
         Click me
-        <MousePointerClick />
       </Button>
     </div>
   );
